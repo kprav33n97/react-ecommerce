1b9696eff3c5414d07cda8af6eb5dcf44ae905de
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-01-20 15:36:31 +0530

    Theme add

--- src/App.js
+++ src/App.js
@@ -14,9 +14,20 @@ const App = () => {
   
   const theme = {
     colors: {
-      bg: "#000",
+      bg: "#f6f8fa",
+      footer_bg: "#0a1435",
+      btn: "rbg(98, 84, 243)",
+      border: "rgba(98, 84, 243, 0.5)",
+      hr: "#fff",
+      gradient: "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+      shadow: "rgba(0, 0, 0, 0.02 ) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
+      shadowSupport: "rbga(0, 0, 0, 0.160) 0px 1px 4px"
     },
-  }
+    media: {
+      mobile: "768px",
+      tab: "998px",
+    },
+  };
 
   return (
     <ThemeProvider theme={theme}>
